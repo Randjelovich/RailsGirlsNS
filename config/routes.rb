6f7233c570b5d8da149eb9ba9c3b697065f46ccb
Danijela Randjelovic <randjelovicdanijela994@gmail.com>
2019-01-20 16:19:11 +0100

    Information about me

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
   resources :bees
-  resources :ideas
+  root to: redirect('/bees')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
